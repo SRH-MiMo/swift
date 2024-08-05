2e1410c04ee2013b25804f9027131118eb59b4a7
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-05 23:07:48 +0900

    init

---
+++ mimo/mimo/mimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mimoApp.swift
+//  mimo
+//
+//  Created by Hyun Seo Jung on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct mimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
